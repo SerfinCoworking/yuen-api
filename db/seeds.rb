fa1581e2c404e242baa7ee0c50d92951f27595ee
eugesma@gmail.com <eugesma@gmail.com>
2021-04-19 22:13:04 -0300

    feat: add some records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+cash_type = AccountType.create(name: 'Efectivo')
+account_type = AccountType.create(name: 'Cuentas')
+AccountType.create(name: 'Tarjeta de crédito')
+AccountType.create(name: 'Tarjeta de débito')
+AccountType.create(name: 'Ahorro')
+AccountType.create(name: 'Tarjeta prepago')
+AccountType.create(name: 'Inversión')
+AccountType.create(name: 'Descubierto bancario')
+AccountType.create(name: 'Seguro')
+AccountType.create(name: 'Otro')
+
+Account.create(name: 'Billetera', account_type: cash_type, user_id: 1)
+Account.create(name: 'MercadoPago', account_type: account_type, user_id: 1)
