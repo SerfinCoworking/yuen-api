daf7fab6be4d218742e37ac21e06b317682743a5
paul <paul.18.sma@gmail.com>
2021-04-25 00:39:00 -0300

    create user module

---
+++ db/migrate/20210425033009_create_users.rb
@@ -0,0 +1,20 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.boolean :email_verified
+      t.string :email
+      t.string :auth0_id
+      t.string :username
+      t.string :phone_number
+      t.boolean :phone_verified
+      t.string :picture
+      t.string :name
+      t.string :nickname
+      t.boolean :blocked
+      t.string :given_name
+      t.string :family_name
+
+      t.timestamps
+    end
+  end
+end
