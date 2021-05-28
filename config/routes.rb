1ed77e0aa714d932781aec491aa335c41b72e05c
eugesma@gmail.com <eugesma@gmail.com>
2021-05-28 20:18:12 -0300

    fix: anidate product category routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   resources :providers
   resources :customers
   resources :stock_locations
-  resources :product_categories
   resources :users
   post 'auth/register', to: 'users#register'
-  resources :products
+  resources :products do
+    collection do
+      resources :product_categories, path: :categories
+    end
+  end
   resources :company_users
   resources :companies
   resources :account_types
