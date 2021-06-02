0ef148d7808dafe5bb9dfffb8d208e41385faa9f
eugesma@gmail.com <eugesma@gmail.com>
2021-06-02 18:33:22 -0300

    fix: permit quantiy per unit in products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,6 +43,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:barcode, :name, :description, :category_id, :unity_id)
+      params.require(:product).permit(:barcode, :name, :description, :category_id, :quantity_per_unit, :unity_id)
     end
 end
