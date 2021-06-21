db79374cdd6382673f1068a098c0db5ef43dcdd8
eugesma@gmail.com <eugesma@gmail.com>
2021-06-20 21:15:06 -0300

    fix: remove scope from unities

--- app/controllers/unities_controller.rb
+++ app/controllers/unities_controller.rb
@@ -3,7 +3,7 @@ class UnitiesController < ApplicationController
 
   # GET /unities
   def index
-    @unities = Unity.by_company(current_user.current_company)
+    @unities = Unity.all
 
     json_response(@unities)
   end
