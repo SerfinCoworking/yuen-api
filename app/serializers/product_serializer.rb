1fdd13915f4b0f1bea2a34c00e42647a7eda615b
eugesma@gmail.com <eugesma@gmail.com>
2021-06-26 11:01:24 -0300

    fix: skip before action authorize

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :category, :unity, :quantity_per_unit, :barcode, :description
+  attributes :id, :name, :category, :category_id, :company_id, :unity, :quantity_per_unit, :barcode, :description
 
   belongs_to :unity
   belongs_to :category
