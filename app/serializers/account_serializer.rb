9c2861e68f74153481836591c45af4ed8af2c744
eugesma@gmail.com <eugesma@gmail.com>
2021-07-04 22:31:14 -0300

    fix: remove user from serializer

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,6 +1,3 @@
 class AccountSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user, :company, :account_alias, :cbu, :description, :balance
-
-  belongs_to :user
-  belongs_to :company
+  attributes :id, :name, :company, :account_alias, :cbu, :description, :balance
 end
