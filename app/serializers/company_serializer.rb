ac0806fb6b38e8fbf6c7bd366f99fa46a650a9c9
eugesma@gmail.com <eugesma@gmail.com>
2021-07-04 23:59:19 -0300

    feat: add web_page to companies

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,12 +1,3 @@
 class CompanySerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :address, :phone, :logo
-
-  has_many :company_users
-  has_many :users, through: :company_users
-  has_many :products
-  has_many :product_categories
-  has_many :stock_locations
-  has_many :accounts
-  has_many :customers
-  has_many :providers
+  attributes :id, :name, :email, :address, :phone, :logo, :web_page
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_224914) do
+ActiveRecord::Schema.define(version: 2021_07_05_025717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_06_29_224914) do
     t.string "logo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "web_page"
   end
 
   create_table "company_users", force: :cascade do |t|
