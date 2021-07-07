3e5a1272b6ed3470ea3295587e7b555645439de6
paul <paul.18.sma@gmail.com>
2021-07-06 21:58:25 -0300

    improve purchase serializer

--- app/serializers/purchase_serializer.rb
+++ app/serializers/purchase_serializer.rb
@@ -1,4 +1,4 @@
 class PurchaseSerializer < ActiveModel::Serializer
-  attributes :id, :provider, :company, :status, :total_price, :reference_number, :request_date,
+  attributes :id, :provider_id, :provider, :status, :total_price, :reference_number, :request_date,
              :price_list_updated_date, :purchase_products
 end
