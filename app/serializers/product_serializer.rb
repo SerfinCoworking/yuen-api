867944ed14b14137c3a9fb7ac70c90a8cf59e500
eugesma@gmail.com <eugesma@gmail.com>
2021-07-17 13:17:19 -0300

    add relationships to product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,7 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :category_id, :company_id, :unity_id, :quantity_per_unit, :barcode,
              :description, :category, :unity
+
+  has_many :purchase_products
+  has_many :purchases, through: :purchase_products
 end
